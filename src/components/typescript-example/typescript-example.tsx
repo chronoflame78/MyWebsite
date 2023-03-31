4273a70ca6b1be953be373476983f391859f5b08
chronoflame78 <gaovanthet555@gmail.com>
2023-03-31 22:47:04 +0700

    add ts example

---
+++ src/components/typescript-example/typescript-example.tsx
@@ -0,0 +1,47 @@
+import { ChangeEventHandler } from "react";
+
+const func: (a: string, b: number, c: boolean) => void = (a, b, c) => {}
+
+interface ISearchBoxProps extends IChangeHandlerProps {
+    className: string;
+    placeHolder?: string;
+}
+
+interface IChangeHandlerProps {
+    onChangeHandler: ChangeEventHandler<HTMLInputElement>
+    //onChangeHandler: (a: object) => void
+}
+
+type SearchBoxProps = {
+    className: string;
+    placeholder?: string;
+    onChangeHandler: ChangeEventHandler<HTMLInputElement>
+}
+
+type CanadianAddress = {
+    street: string;
+    province: string;
+}
+
+type USAddress = {
+    street: string;
+    state: string;
+}
+
+type NorthAmericanAddress = CanadianAddress | USAddress;
+
+const Address: NorthAmericanAddress = {
+    street: 'adas',
+    state: 'adasd'
+}
+
+const SearchBox = ({className, placeHolder, onChangeHandler}: ISearchBoxProps) => (
+    <input
+        className={className}
+        type='search'
+        placeholder={placeHolder}
+        onChange={(e) => onChangeHandler(e)}
+    />
+)
+
+export default SearchBox;
