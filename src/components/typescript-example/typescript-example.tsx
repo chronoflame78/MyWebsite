e8c2d487e9489d05ebed9b8f229064a9320d2f2e
chronoflame78 <gaovanthet555@gmail.com>
2023-04-11 23:03:32 +0700

    typescript enum ex

--- src/components/typescript-example/typescript-example.tsx
+++ src/components/typescript-example/typescript-example.tsx
@@ -46,3 +46,12 @@ const SearchBox = ({className, placeHolder, onChangeHandler}: ISearchBoxProps) =
 )
 
 export default SearchBox;
+
+
+
+export enum CATEGORIES_ACTION_TYPES {
+    FETCH_CATEGORIES_START = 'category/FETCH_CATEGORIES_START',
+    FETCH_CATEGORIES_SUCCESS = 'category/FETCH_CATEGORIES_SUCCESS',
+    FETCH_CATEGORIES_FAILED = 'category/FETCH_CATEGORIES_FAILED',
+};
+  
