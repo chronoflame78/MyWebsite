0625690ce9621d6aac5261a1fc2671396d5b014a
chronoflame78 <gaovanthet555@gmail.com>
2023-05-03 22:05:37 +0700

    update

--- src/components/typescript-example/algorithms.js
+++ src/components/typescript-example/algorithms.js
@@ -181,3 +181,34 @@ var intToRoman = function(num) {
     }
     return result;
 };
+
+
+//https://leetcode.com/problems/roman-to-integer/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const items = {
+    I:1,
+    V:5,
+    X:10,
+    L:50,
+    C:100,
+    D:500,
+    M:1000
+  };
+  let total = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (items[s[i]] < items[s[i+1]]) {
+      total += items[s[i+1]] - items[s[i]];
+      i++;
+    } else {
+      total += items[s[i]];
+    }
+  }
+
+  return total;
+
+};
