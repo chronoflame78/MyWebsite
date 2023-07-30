9508b1f80b06c5aa600ed82309735a2c01a23d43
chronoflame78 <gaovanthet555@gmail.com>
2023-07-30 22:15:59 +0700

    update toppane, add home page

--- src/components/app/topPane.tsx
+++ src/components/app/topPane.tsx
@@ -1,27 +1,34 @@
-import { Location, useLocation } from "react-router-dom";
+import { Location, redirect, useLocation, useNavigate } from "react-router-dom";
 import classNames from "classnames";
 import { ABOUT_PATH, CONTACT_PATH, HOME_PATH } from "../../utils/constant";
+import * as SVGComponents from "../../styles/svgIcons";
 
-function TopPane() {
+function TopPane(props) {
   const location: Location = useLocation();
+  const navigate = useNavigate();
   const { pathname } = location;
 
+  const redirectToHomePage = () => {
+    navigate("/home");
+  }
+
   return (
-    <nav className="top-pane d-flex align-items-center justify-content-between px-3 py-4">
-      <div>
-        <a href="#">Your Brand</a>
+    <nav className="top-pane container d-flex align-items-center justify-content-between py-4">
+      <div className="h-cursor" onClick={() => redirectToHomePage()}>
+        <SVGComponents.IconLeo className="leo-icon mr-2"/>
+        <span className="f-20 font-weight-bold primary-color-2">Leo</span>
       </div>
       {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button> */}
       <div className="top-pane-nav">
         <ul className="navbar-ul">
-          <li className={classNames("nav-item", {"active": pathname === HOME_PATH})}>
+          <li className={classNames("nav-item mr-2", {"active": pathname === HOME_PATH})}>
             <a className="nav-link" href={HOME_PATH}>
               Home
             </a>
           </li>
-          <li className={classNames("nav-item", {"active": pathname === ABOUT_PATH})}>
+          <li className={classNames("nav-item mr-2", {"active": pathname === ABOUT_PATH})}>
             <a className="nav-link" href={ABOUT_PATH}>
               About
             </a>
--- src/components/home/homePage.tsx
+++ src/components/home/homePage.tsx
@@ -2,8 +2,25 @@ function HomePage() {
 
   return (
     <>
-      <div>
-       <p>Home page</p>
+      <div className="container d-flex align-items-center home-page-wrapper">
+        <div className="row w-100">
+          <div className="d-flex flex-direction-column col-sm-12">
+            <h2 className="f-60 font-weight-bold hello-text">Hello,</h2>
+            <h2 className="f-90 font-weight-bold py-4">I Am Leo</h2>
+            <h5 className="f-32 font-weight-bold pb-4 primary-color-2">Front-end developer</h5>
+            <div className="description-text">
+            Must explain to how all this mistaken idea denouncing pleasure pain the system and expound the actua.
+            </div>
+            <button className="f-18 btn">
+              More about me
+            </button>
+          </div>
+          <div className="col-sm-12">
+
+          </div>
+        </div>
+        
+        
       </div>
     </>
   )
