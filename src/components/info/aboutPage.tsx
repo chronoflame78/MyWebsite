3a193307f538bb6071f7fa5bb4405e7db6d5bce7
chronoflame78 <gaovanthet555@gmail.com>
2023-08-04 22:18:35 +0700

    update about page

--- src/components/info/aboutPage.tsx
+++ src/components/info/aboutPage.tsx
@@ -16,22 +16,60 @@ function AboutPage() {
     return (
       <>
       <div className="container d-flex align-items-center about-page-wrapper pt-4">
-        <div className="w-100 d-flex align-items-center flex-direction-column pt-5">
+        <div className="w-100 d-flex flex-direction-column pt-4">
           
-            <h2 className="f-48 font-weight-bold">My skills</h2>
+            <div>
+              <p className="f-20 font-weight-bold text-gray mb-1">MY SKILLS</p>
+              <h2 className="f-60 font-weight-bold pb-4 text-gray">Technologies.</h2>
+            </div>
+            
 
-            <div className="w-100">
+            <div className="flex-fill pl-4">
+              <div className="d-flex align-items-center justify-content-between p-3 mb-4">
+                <div className="card-item py-4">
+                  <div>
+                    <SVGComponents.LogoReact className="logo-react"/>
+                  </div>
+                  <div className="f-20 font-weight-bold">
+                    React
+                  </div>
+                </div>
+                <div className="card-item py-4">
+                  <div>
+                    <SVGComponents.LogoJS className="logo-javascript"/>
+                  </div>
+                  <div className="f-20 font-weight-bold">
+                    Javascript
+                  </div>
+                </div>
+                <div className="card-item py-4">
+                  <div>
+                    <SVGComponents.LogoTS className="logo-typescript"/>
+                  </div>
+                  <div className="f-20 font-weight-bold">
+                    Typescript
+                  </div>
+                </div>
+                <div className="card-item py-4">
+                  <div>
+                    <SVGComponents.LogoRedux className="logo-redux"/>
+                  </div>
+                  <div className="f-20 font-weight-bold">
+                    Redux
+                  </div>
+                </div>
+              </div>
               <div className="d-flex align-items-center justify-content-between p-3">
-                <div className="card-item">
+                <div className="card-item py-4">
 
                 </div>
-                <div className="card-item">
+                <div className="card-item py-4">
 
                 </div>
-                <div className="card-item">
+                <div className="card-item py-4">
 
                 </div>
-                <div className="card-item">
+                <div className="card-item py-4">
 
                 </div>
               </div>
@@ -42,12 +80,28 @@ function AboutPage() {
         
         
       </div>
-      <div className="shape1-container">
+      <div className="container d-flex align-items-center about-page-wrapper height-100vh pt-4">
+        <div className="w-100 d-flex flex-direction-column pt-4 align-items-center">
+          
+            
+              <h2 className="f-48 font-weight-bold mb-4 text-center">My Work Experience</h2>
+              <p className="f-18 font-weight-bold pb-4 work-des-txt text-center">
+                Must explain to you how all this mistaken idea of denouncing pleasure born and give you a complete account the system
+              </p>
+            
+            
+          
+
+        </div>
+        
+        
+      </div>
+      {/* <div className="shape1-container">
         <SVGComponents.Shape1 className="shape1-svg" />
       </div>
       <div className="shape2-container">
         <SVGComponents.Shape2 className="shape2-svg" />
-      </div>
+      </div> */}
       <div className="shape3-container">
         <SVGComponents.Shape3 className="shape3-svg" />
       </div>
