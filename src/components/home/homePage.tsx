17a961d2c11da92c5be221b2c7ed382531400ae6
chronoflame78 <gaovanthet555@gmail.com>
2023-08-25 22:35:52 +0700

    update mobile view homepage

--- src/components/home/homePage.tsx
+++ src/components/home/homePage.tsx
@@ -45,37 +45,39 @@ function HomePage() {
 
   return (
     <>
-      <div className="container d-flex align-items-center home-page-wrapper pt-4">
+      <div className="container d-flex align-items-center home-page-wrapper">
         <div className="row w-100">
-          <div className="d-flex flex-direction-column col-sm-12">
-            <h2 className="f-60 font-weight-bold hello-text">Hello,</h2>
-            <h2 className="f-90 font-weight-bold py-4">I Am Leo.</h2>
-            <h5 className="f-32 font-weight-bold pb-4 primary-color-2">Front-end developer</h5>
-            <div className="description-text pb-4">
+          <div className="d-flex flex-direction-column col-sm-12 info-container">
+            <h2 className="font-weight-bold hello-text">Hello,</h2>
+            <h2 className="font-weight-bold name-text">I Am Leo.</h2>
+            <h5 className="font-weight-bold primary-color-2 title-text">Front-end developer</h5>
+            <div className="description-text">
               Front-end Developer with over 2 years of experience. Experienced with JavaScript and React framework.
             </div>
-            <button type="button" onClick={() => navigateToAbout()} className="f-18 btn btn-primary-custom font-weight-bold py-3 px-4 d-flex align-items-center">
-              More about me <FontAwesomeIcon className="ml-3" icon="arrow-right" size="1x" color="#ffffff" />
-            </button>
+            <div className="button-container">
+              <button type="button" onClick={() => navigateToAbout()} className="f-18 btn btn-primary-custom font-weight-bold py-3 px-4 d-flex align-items-center">
+                More about me <FontAwesomeIcon className="ml-3" icon="arrow-right" size="1x" color="#ffffff" />
+              </button>
+            </div>
           </div>
-          <div className="col-sm-12">
+          <div className="col-sm-12 image-container">
             <img className="main-image" src={myImage} alt=""/>
-            <div className="react-logo-container">
+            <div className="react-logo-container logo-container">
               <div className="bg-container">
                 <SVGComponents.LogoReact className="react-logo"/>
               </div>
             </div>
-            <div className="js-logo-container">
+            <div className="js-logo-container logo-container">
               <div className="bg-container">
                 <SVGComponents.LogoJS className="js-logo"/>
               </div>
             </div>
-            <div className="redux-logo-container">
+            <div className="redux-logo-container logo-container">
               <div className="bg-container">
                 <SVGComponents.LogoRedux className="redux-logo"/>
               </div>
             </div>
-            <div className="sass-logo-container">
+            <div className="sass-logo-container logo-container">
               <div className="bg-container">
                 <SVGComponents.LogoSass className="sass-logo"/>
               </div>
